efb229dd283223259ce48dd006eaca9463d1eafb
Huda Yousif <hjdoski@gmail.com>
2019-08-17 21:17:18 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,4 @@
-meal_choice(veg1, veg2, protein = "meat")
+def meal_choice(veg1, veg2, protein = "meat")
    puts "what a nutrious meal"
    puts "A placte with a #{protein} with a #{veg1} and with a #{veg2}."
-end
+--
